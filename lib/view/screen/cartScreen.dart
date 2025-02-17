c412438792cfa45b4958f5c2f1fe6b9903e7329b
Abdulrahman <abdulrahman@gamil.com>
2025-02-17 18:34:05 +0200

    cart screen retouch
    move the order button to another file

--- lib/view/screen/cartScreen.dart
+++ lib/view/screen/cartScreen.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:ecommerce_app_w/controller/cart_controller.dart';
+import 'package:ecommerce_app_w/core/class/handlingdataviewW.dart';
 import 'package:ecommerce_app_w/core/constant/color.dart';
 import 'package:ecommerce_app_w/core/constant/imageassets.dart';
 import 'package:ecommerce_app_w/data/model/cart_item.dart';
 import 'package:ecommerce_app_w/view/widget/cart/cart_item_list.dart';
+import 'package:ecommerce_app_w/view/widget/cart/place_order_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -26,70 +28,20 @@ class CartScreen extends StatelessWidget {
             ),
           ),
         ),
-        body: Obx(() {
-          if (controller.cartItems.isEmpty) {
-            return Center(
-              child: Text(
-                "No items in your cart yet!",
-                style: TextStyle(fontSize: 18, color: Colors.grey[600]),
-              ),
-            );
-          }
-          return ListView(
-            children: [
-              ...List.generate(controller.cartItems.length, (index) {
-                return SizedBox(
-                  child: CartItemList(
-                    cartItem: controller.cartItems[index],
-                  ),
-                );
-              }),
-            ],
-          );
-        }),
-        bottomNavigationBar: SafeArea(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "Total price",
-                      style: TextStyle(fontWeight: FontWeight.bold),
+        body: HandlingDataViewW(
+            statusRequest: controller.statusRequest,
+            widget: ListView(
+              children: [
+                ...List.generate(controller.cartItems.length, (index) {
+                  return SizedBox(
+                    child: CartItemList(
+                      cartItem: controller.cartItems[index],
                     ),
-                    Text(
-                      "${controller.totalPrice} \$",
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: TextButton(
-                  onPressed: () {
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all<Color>(
-                        AppColor.primaryColor),
-                    minimumSize: WidgetStateProperty.all<Size>(
-                        const Size(double.infinity, 50)),
-                  ),
-                  child: const Text(
-                    "Place order",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+                  );
+                }),
+              ],
+            )),
+        bottomNavigationBar: const PlaceOrderButton(),
       );
     });
   }
